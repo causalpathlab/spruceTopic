19465dccce8c8b4ea608c08c5e97f12ba6ed2120
sishirsubedi <subedisishir@gmail.com>
2022-01-15 19:40:14 -0800

    testing vae

--- scripts/dlearn/ae.py
+++ scripts/dlearn/ae.py
@@ -20,9 +20,32 @@ class Encoder(nn.Module):
 		self.linear1 = nn.Linear(input_dims, 128)
 		self.linear2 = nn.Linear(128, latent_dims)
 	def forward(self, x):
-		x = torch.flatten(x, start_dim=1)
-		x = F.relu(self.linear1(x))
-		return self.linear2(x)
+		output = F.relu(self.linear1(x))
+		z = F.relu(self.linear2(output))
+		return z
+
+class VariationalEncoder(nn.Module):
+	def __init__(self, input_dims, latent_dims):
+		super(VariationalEncoder, self).__init__()
+		self.linear1 = nn.Linear(input_dims, 128)
+		self.linear2 = nn.Linear(128, latent_dims)
+		self.linear3 = nn.Linear(128, latent_dims)
+
+		## TODO need to look up pytorch documentation for cuda sampling call
+		self.N = torch.distributions.Normal(0, 1)
+		self.N.loc = self.N.loc.cuda() 
+		self.N.scale = self.N.scale.cuda()
+		self.kl = 0
+
+	def forward(self, x):
+		output = F.relu(self.linear1(x))
+		mu =  self.linear2(output)
+		## TODO do we need to pass sigma from relu as well if exp is used
+		sigma = torch.exp(self.linear3(output))
+		z = mu + sigma*self.N.sample(mu.shape)
+		self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+		return z
+
 
 class Decoder(nn.Module):
 	def __init__(self, input_dims, latent_dims):
@@ -30,14 +53,23 @@ class Decoder(nn.Module):
 		self.linear1 = nn.Linear(latent_dims, 128)
 		self.linear2 = nn.Linear(128, input_dims)
 	def forward(self, z):
-		z = F.relu(self.linear1(z))
-		z = torch.sigmoid(self.linear2(z))
-		return z
+		output = F.relu(self.linear1(z))
+		x_hat = F.relu(self.linear2(output))
+		return x_hat
 
 class Autoencoder(nn.Module):
 	def __init__(self,input_dims,latent_dims):
 		super(Autoencoder, self).__init__()
 		self.encoder = Encoder(input_dims,latent_dims)
+		self.decoder = Decoder(input_dims,latent_dims)
+	def forward(self, x):
+		z = self.encoder(x)
+		return self.decoder(z)
+
+class VariationalAutoencoder(nn.Module):
+	def __init__(self,input_dims,latent_dims):
+		super(VariationalAutoencoder, self).__init__()
+		self.encoder = VariationalEncoder(input_dims,latent_dims)
 		self.decoder = Decoder(input_dims,latent_dims)
 	def forward(self, x):
 		z = self.encoder(x)
@@ -58,21 +90,37 @@ def load_data(x,y,device):
 	return DataLoader(TabularDataset(x,y), batch_size=64, shuffle=True)
 
 def train(autoencoder, data,device, epochs=300):
-	# opt = torch.optim.Adam(autoencoder.parameters())
-	opt = torch.optim.SGD(autoencoder.parameters(),lr=0.01)
-	# loss = nn.MSELoss(reduction="sum")
-	train_loss = []
+	opt = torch.optim.Adam(autoencoder.parameters())
+	mse = nn.MSELoss()
 	for epoch in range(epochs):
+		loss = 0
 		for indx,(x,y) in enumerate(data):
 			x = x.to(device)
 			opt.zero_grad()
 			x_hat = autoencoder(x)
-			loss = ((x - x_hat)**2).sum()
-			train_loss.append(loss.item())
-			loss.backward()
+			train_loss = mse(x_hat,x)
+			train_loss.backward()
 			opt.step()
-		if epoch % 30 == 0:        
-			print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss[-1]))
+			loss += train_loss.item()
+		if epoch % 30 == 0:  
+			print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, loss/len(data)))
 	return autoencoder
 
+
+def vaetrain(autoencoder, data,device, epochs=50):
+	opt = torch.optim.Adam(autoencoder.parameters())
+	mse = nn.MSELoss()
+	for epoch in range(epochs):
+		loss = 0
+		for indx,(x,y) in enumerate(data):
+			x = x.to(device)
+			opt.zero_grad()
+			x_hat = autoencoder(x)
+			train_loss = mse(x_hat,x) + autoencoder.encoder.kl
+			train_loss.backward()
+			opt.step()
+			loss += train_loss.item()
+		if epoch % 5 == 0:  
+			print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, loss/len(data)))
+	return autoencoder
 
--- scripts/wip.py
+++ scripts/wip.py
@@ -11,24 +11,43 @@ df = pd.read_csv("../output/cd4_c13_c18_counts.txt.gz",sep="\t")
 df = preprocess.filter_genes(df)
 
 ###
+## with cell type
+# y = np.array([1 if "c13" in x else 0 for x in df.iloc[:,-1]])
+# pca_check.run_pca(df.iloc[:,1:df.shape[1]-1],y)
+
+## with tissue type
 # pca_check.run_pca(df.iloc[:,1:df.shape[1]-1],df.iloc[:,-1].values)
 
-###
+# ###
 device = ae.torch.device('cuda' if ae.torch.cuda.is_available() else 'cpu')
 input_dims = df.iloc[:,1:-1].shape[1]
 latent_dims = 32
-autoencoder = ae.Autoencoder(input_dims,latent_dims).to(device)
-y = pd.factorize(df.iloc[:,-1])[0]
+
+# autoencoder = ae.Autoencoder(input_dims,latent_dims).to(device)
+# #with tissue type 
+# y = pd.factorize(df.iloc[:,-1])[0]
+# #with cell type
+# y = np.array([1 if "c13" in x else 0 for x in df.iloc[:,-1]])
+# data = ae.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
+# autoencoder = ae.train(autoencoder,data,device)
+
+
+autoencoder = ae.VariationalAutoencoder(input_dims,latent_dims).to(device)
+# #with tissue type 
+# y = pd.factorize(df.iloc[:,-1])[0]
+# #with cell type
+y = np.array([1 if "c13" in x else 0 for x in df.iloc[:,-1]])
 data = ae.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
-autoencoder = ae.train(autoencoder,data,device)
+autoencoder = ae.vaetrain(autoencoder,data,device)
+
 
 # def plot_latent(autoencoder, data, num_batches=100):
-# for i, (x,y) in enumerate(data):
-# 	z = autoencoder.encoder(x.to(device))
-# 	z = z.to('cpu').detach().numpy()
-# 	plt.scatter(z[:, 0], z[:, 1],c=y, cmap='tab10')
-# 	if i > 100:
-# 		break
-# plt.savefig("../output/ae.png");plt.close()
+for i, (x,y) in enumerate(data):
+	z = autoencoder.encoder(x.to(device))
+	z = z.to('cpu').detach().numpy()
+	plt.scatter(z[:, 0], z[:, 1],c=y, cmap='tab10')
+	if i > 100:
+		break
+plt.savefig("../output/vae.png");plt.close()
 
 
