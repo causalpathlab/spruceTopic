6c0db9aef310095c806bd9613c1b363c905b4c32
sishirsubedi <subedisishir@gmail.com>
2022-01-20 18:49:45 -0800

    etm-working version, needs data testing

--- scripts/dlearn/etm.py
+++ scripts/dlearn/etm.py
@@ -1,7 +1,11 @@
 
+from cmath import log
 import torch; torch.manual_seed(0)
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 
 
 class Stacklayers(nn.Module):
@@ -9,75 +13,53 @@ class Stacklayers(nn.Module):
 		super(Stacklayers, self).__init__()
 		self.layers = nn.ModuleList()
 		self.input_size = input_size
-		for next_size,activfunc in layers:
+		for next_size in layers:
 			self.layers.append(nn.Linear(input_size,next_size))
 			input_size = next_size
-			self.layers.append(self.get_activation(activfunc))
+			self.layers.append(self.get_activation())
 		
 	def forward(self, input_data):
 		for layer in self.layers:
 			input_data = layer(input_data)
 		return input_data
 
-	def get_activation(self, act):
-		if act == 'tanh':
-			act = nn.Tanh()
-		elif act == 'relu':
-			act = nn.ReLU()
-		elif act == 'softplus':
-			act = nn.Softplus()
-		elif act == 'rrelu':
-			act = nn.RReLU()
-		elif act == 'leakyrelu':
-			act = nn.LeakyReLU()
-		elif act == 'elu':
-			act = nn.ELU()
-		elif act == 'selu':
-			act = nn.SELU()
-		elif act == 'glu':
-			act = nn.GLU()
-		else:
-			print('Defaulting to tanh activations...')
-			act = nn.Tanh()
-		return act 
-
+	def get_activation(self):
+		return nn.ReLU()
 
 class ETMEncoder(nn.Module):
 	def __init__(self,input_dims,latent_dims,layers):
 		super(ETMEncoder, self).__init__()
 		self.fc = Stacklayers(input_dims,layers)
-		hidden_dims = layers[len(layers)][0]
+		hidden_dims = layers[len(layers)-1]
+		self.latent_dim = latent_dims
 		self.z_mu = nn.Linear(hidden_dims,latent_dims)
 		self.z_var = nn.Linear(hidden_dims,latent_dims)
-		self.latent_dim = latent_dims
 
-	def forward(self, xx):
-		# xx = ## get normalized of xx
-		ss = self.fc(torch.log1p(xx))
-		mm = self.z_mu(ss)
-		lv = torch.exp(self.z_var(ss))
-		z = self.normal_sample(mm,lv)
-		return [z,mm,lv]
-	
 	def normal_sample(self,mean,logvar):
-		std = torch.exp(logvar/2.)
+		std = torch.exp(0.5*logvar)
 		eps = torch.randn_like(std)
 		return eps.mul_(std).add_(mean)
 
-	def kl_loss(self,mean,logvar):
-		return  -0.5 * torch.sum(1. + logvar - mean.pow(2) - logvar.exp(), dim=-1).mean()
-	
+
+	def forward(self, xx):
+		## TODO normalization ? q_theta = qtheta/torch.sum(xx,dim-1)
+		q_theta = self.fc(torch.log1p(xx))
+		mu_theta = self.z_mu(q_theta)
+		logsigma_theta = self.z_var(q_theta)
+
+		zz = self.normal_sample(mu_theta,logsigma_theta)
+
+		return zz, mu_theta,logsigma_theta
+
 
 class ETMDecoder(nn.Module):
 	def __init__(self,out_dims,latent_dims,jitter=.1):
 		super(ETMDecoder, self).__init__()
-		self.lbeta = nn.parameter(torch.randn(latent_dims,out_dims)*jitter)
-		self.beta = nn.LogSoftmax(2)
-		self.hid = nn.LogSoftmax(2)
+		self.q_beta = nn.Parameter(torch.randn(latent_dims,out_dims)*jitter)
 
 	def forward(self, zz):
-		beta = self.beta(self.lbeta)
-		hh = self.hid(zz)
+		beta = F.softmax(self.q_beta,dim=-1)
+		hh = F.softmax(zz,dim=-1)
 		return torch.mm(torch.exp(hh),torch.exp(beta))
 
 class ETM(nn.Module):
@@ -87,13 +69,46 @@ class ETM(nn.Module):
 		self.decoder = ETMDecoder(out_dims,latent_dims)
 		
 	def forward(self,xx):
-		zz = self.encoder(xx)
+		zz,mu_theta,logsigma_theta = self.encoder(xx)
 		recon = self.decoder(zz)
-		return [recon,zz[1],zz[2]]
-		
-mlnn = Stacklayers(1500, \
-[(128,"relu"),\
-(100,"relu"),\
-(50,"relu"),\
-(32,"relu")])
-print(mlnn)
+		return recon,mu_theta,logsigma_theta
+
+def etm_llik(xx,pr, eps=1e-8):
+	# return torch.sum(xx* torch.log(pr+eps),dim=-1)
+	recon_loss = -(pr * xx).sum(1)
+	return recon_loss.mean()
+
+def kl_loss(mean,logvar):
+	return  -0.5 * torch.sum(1. + logvar - mean.pow(2) - logvar.exp(), dim=-1).mean()
+
+class TabularDataset(Dataset):
+	def __init__(self, x,y):
+		self.X = x
+		self.y = y	
+	def __len__(self):
+		return len(self.X)
+	def __getitem__(self, idx):
+		return [self.X[idx], self.y[idx]]
+
+def load_data(x,y,device):
+	x = x.astype('float32')
+	x = torch.from_numpy(x).to(device)
+	return DataLoader(TabularDataset(x,y), batch_size=64, shuffle=True)
+
+def train(etm, data,device, epochs=300):
+	opt = torch.optim.Adam(etm.parameters())
+	for epoch in range(epochs):
+		loss = 0
+		for indx,(x,y) in enumerate(data):
+			x = x.to(device)
+			opt.zero_grad()
+			x_hat,mu_theta,logsigma_theta = etm(x)
+			loglikloss = etm_llik(x,x_hat)
+			klloss = kl_loss(mu_theta,logsigma_theta)
+			train_loss = loglikloss + klloss
+			train_loss.backward()
+			opt.step()
+			loss += train_loss.item()
+		if epoch % 30 == 0:  
+			print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, loss/len(data)))
+	return etm
--- scripts/wip.py
+++ scripts/wip.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import preprocess 
 import component_check 
-from dlearn import ae
+from dlearn import etm
 import importlib
 
 data="cd4_cd8_500cells_per_tissue_counts.txt.gz"
@@ -15,50 +15,14 @@ df = preprocess.filter_minimal(df)
 y = np.array([1 if "CD4" in x else 0 for x in df.iloc[:,-1]])
 # y = pd.factorize(df.iloc[:,-1])[0]
 
-component_check.run_component_analysis(df.iloc[:,1:df.shape[1]-1],y,"tsne")
+# component_check.run_component_analysis(df.iloc[:,1:df.shape[1]-1],y,"tsne")
 
-#### ae tests
-device = ae.torch.device('cuda' if ae.torch.cuda.is_available() else 'cpu')
+#### etm tests
+device = etm.torch.device('cuda' if etm.torch.cuda.is_available() else 'cpu')
 input_dims = df.iloc[:,1:-1].shape[1]
+layers = [128,32]
 latent_dims = 32
 
-autoencoder = ae.Autoencoder(input_dims,latent_dims).to(device)
-data = ae.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
-autoencoder = ae.train(autoencoder,data,device)
-ae.plot_ae_components(data,autoencoder,device,"cd4_cd8_ae")
-
-
-df_z = pd.concat([df[["cell"]],ae.get_encoded_z(df,autoencoder,device)],axis=1)
-df_z = df_z.set_index("cell")
-
-from sklearn.neighbors import NearestNeighbors
-import igraph
-
-nbrs = NearestNeighbors(n_neighbors=10).fit(df_z)
-nbrs_mat = nbrs.kneighbors_graph(df_z).toarray()
-np.fill_diagonal(nbrs_mat, 0)
-
-g = igraph.Graph.Adjacency((nbrs_mat > 0).tolist())
-g.es['weight'] = nbrs_mat[nbrs_mat.nonzero()]
-g.vs['label'] = df_z.index
-
-
-visual_style = {}
-# Define colors used for outdegree visualization
-colours = ['#fecc5c', '#a31a1c']
-# Set bbox and margin
-visual_style["bbox"] = (3000,3000)
-visual_style["margin"] = 17
-# Set vertex colours
-visual_style["vertex_color"] = 'grey'
-# Set vertex size
-visual_style["vertex_size"] = 20
-# Set vertex lable size
-visual_style["vertex_label_size"] = 0
-# Don't curve the edges
-visual_style["edge_curved"] = False
-# Set the layout
-my_layout = g.layout_fruchterman_reingold()
-visual_style["layout"] = my_layout
-# Plot the graph
-igraph.plot(g, target='../output/interaction.pdf', **visual_style)
+model = etm.ETM(input_dims,input_dims,latent_dims,layers).to(device)
+data = etm.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
+model = etm.train(model,data,device)
