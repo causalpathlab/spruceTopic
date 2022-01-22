d1ada8214e0e7773061eac0daeebaba905fc064a
sishirsubedi <subedisishir@gmail.com>
2022-01-22 10:06:47 -0800

    fixed beta issue - hopefully working ver 1.0

--- scripts/dlearn/etm.py
+++ scripts/dlearn/etm.py
@@ -1,5 +1,3 @@
-
-from cmath import log
 import torch; torch.manual_seed(0)
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,16 +5,27 @@ import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
+def parameterize(mean,lnvar):
+	sig = torch.exp(lnvar/2.)
+	eps = torch.randn_like(sig)
+	return eps.mul_(sig).add_(mean)
+
+def etm_llik(xx,pr, eps=1e-8):
+	return torch.sum(xx * torch.log(pr+eps),dim=-1)
+
+def kl_loss(mean,lnvar):
+	return  -0.5 * torch.sum(1. + lnvar - torch.pow(mean,2) - torch.exp(lnvar), dim=-1)
 
 class Stacklayers(nn.Module):
 	def __init__(self,input_size,layers):
 		super(Stacklayers, self).__init__()
 		self.layers = nn.ModuleList()
 		self.input_size = input_size
-		for next_size in layers:
-			self.layers.append(nn.Linear(input_size,next_size))
-			input_size = next_size
+		for next_l in layers:
+			self.layers.append(nn.Linear(self.input_size,next_l))
 			self.layers.append(self.get_activation())
+			nn.BatchNorm1d(next_l)
+			self.input_size = next_l
 		
 	def forward(self, input_data):
 		for layer in self.layers:
@@ -32,34 +41,28 @@ class ETMEncoder(nn.Module):
 		self.fc = Stacklayers(input_dims,layers)
 		hidden_dims = layers[len(layers)-1]
 		self.latent_dim = latent_dims
-		self.z_mu = nn.Linear(hidden_dims,latent_dims)
-		self.z_var = nn.Linear(hidden_dims,latent_dims)
-
-	def normal_sample(self,mean,logvar):
-		std = torch.exp(0.5*logvar)
-		eps = torch.randn_like(std)
-		return eps.mul_(std).add_(mean)
-
+		self.z_mean = nn.Linear(hidden_dims,latent_dims)
+		self.z_lnvar = nn.Linear(hidden_dims,latent_dims)
 
 	def forward(self, xx):
-		## TODO normalization ? q_theta = qtheta/torch.sum(xx,dim-1)
-		q_theta = self.fc(torch.log1p(xx))
-		mu_theta = self.z_mu(q_theta)
-		logsigma_theta = self.z_var(q_theta)
-
-		zz = self.normal_sample(mu_theta,logsigma_theta)
-
-		return zz, mu_theta,logsigma_theta
+		xx = xx/torch.sum(xx,dim=-1,keepdim=True)
+		ss = self.fc(torch.log1p(xx))
+		mm = self.z_mean(ss)
+		lv = torch.clamp(self.z_lnvar(ss),-4.0,4.0)
+		z = parameterize(mm,lv)
+		return z, mm,lv
 
 
 class ETMDecoder(nn.Module):
 	def __init__(self,out_dims,latent_dims,jitter=.1):
 		super(ETMDecoder, self).__init__()
-		self.q_beta = nn.Parameter(torch.randn(latent_dims,out_dims)*jitter)
+		self.lbeta = nn.Parameter(torch.randn(latent_dims,out_dims)*jitter)
+		self.beta = nn.LogSoftmax()
+		self.hid = nn.LogSoftmax()
 
 	def forward(self, zz):
-		beta = F.softmax(self.q_beta,dim=-1)
-		hh = F.softmax(zz,dim=-1)
+		beta = self.beta(self.lbeta)
+		hh = self.hid(zz)
 		return torch.mm(torch.exp(hh),torch.exp(beta))
 
 class ETM(nn.Module):
@@ -69,17 +72,10 @@ class ETM(nn.Module):
 		self.decoder = ETMDecoder(out_dims,latent_dims)
 		
 	def forward(self,xx):
-		zz,mu_theta,logsigma_theta = self.encoder(xx)
-		recon = self.decoder(zz)
-		return recon,mu_theta,logsigma_theta
+		zz,mean,lv = self.encoder(xx)
+		pr = self.decoder(zz)
+		return pr,mean,lv
 
-def etm_llik(xx,pr, eps=1e-8):
-	# return torch.sum(xx* torch.log(pr+eps),dim=-1)
-	recon_loss = -(pr * xx).sum(1)
-	return recon_loss.mean()
-
-def kl_loss(mean,logvar):
-	return  -0.5 * torch.sum(1. + logvar - mean.pow(2) - logvar.exp(), dim=-1).mean()
 
 class TabularDataset(Dataset):
 	def __init__(self, x,y):
@@ -93,19 +89,19 @@ class TabularDataset(Dataset):
 def load_data(x,y,device):
 	x = x.astype('float32')
 	x = torch.from_numpy(x).to(device)
-	return DataLoader(TabularDataset(x,y), batch_size=64, shuffle=True)
+	return DataLoader(TabularDataset(x,y), batch_size=200, shuffle=True)
 
 def train(etm, data,device, epochs=300):
-	opt = torch.optim.Adam(etm.parameters())
+	opt = torch.optim.Adam(etm.parameters(),lr=0.001)
 	for epoch in range(epochs):
 		loss = 0
 		for indx,(x,y) in enumerate(data):
 			x = x.to(device)
 			opt.zero_grad()
-			x_hat,mu_theta,logsigma_theta = etm(x)
-			loglikloss = etm_llik(x,x_hat)
-			klloss = kl_loss(mu_theta,logsigma_theta)
-			train_loss = loglikloss + klloss
+			recon,mean,lnvar = etm(x)
+			loglikloss = etm_llik(x,recon)
+			kl = kl_loss(mean,lnvar)
+			train_loss = torch.mean(kl-loglikloss).to("cpu")
 			train_loss.backward()
 			opt.step()
 			loss += train_loss.item()
--- scripts/wip.py
+++ scripts/wip.py
@@ -3,9 +3,11 @@ import numpy as np
 import preprocess 
 import component_check 
 from dlearn import etm
+from dlearn import ae
 import importlib
 
 data="cd4_cd8_500cells_per_tissue_counts.txt.gz"
+# data="lda_test_data.txt"
 df = pd.read_csv("../output/"+data,sep="\t")
 df = df[ ["cell"]+\
         [x for x in df.columns if x not in["cell","sample"]]+\
@@ -15,14 +17,27 @@ df = preprocess.filter_minimal(df)
 y = np.array([1 if "CD4" in x else 0 for x in df.iloc[:,-1]])
 # y = pd.factorize(df.iloc[:,-1])[0]
 
-# component_check.run_component_analysis(df.iloc[:,1:df.shape[1]-1],y,"tsne")
+# component_check.run_component_analysis(df.iloc[:,1:df.shape[1]-1],y,"pca")
 
 #### etm tests
 device = etm.torch.device('cuda' if etm.torch.cuda.is_available() else 'cpu')
 input_dims = df.iloc[:,1:-1].shape[1]
-layers = [128,32]
-latent_dims = 32
+layers = [128,16]
+latent_dims = 16
 
 model = etm.ETM(input_dims,input_dims,latent_dims,layers).to(device)
 data = etm.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
+print(model)
 model = etm.train(model,data,device)
+
+
+
+#### ae tests
+# device = ae.torch.device('cuda' if ae.torch.cuda.is_available() else 'cpu')
+# input_dims = df.iloc[:,1:-1].shape[1]
+# latent_dims = 32
+
+# autoencoder = ae.Autoencoder(input_dims,latent_dims).to(device)
+# data = ae.load_data(df.iloc[:,1:-1].to_numpy(),y,device)
+# autoencoder = ae.train(autoencoder,data,device)
+# ae.plot_ae_components(data,autoencoder,device,"c13_18_all_ae")
