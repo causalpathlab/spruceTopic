b7311ed9d6444924bcca66e8fe0e9a8bc88ff7b0
sishirsubedi <subedisishir@gmail.com>
2022-06-15 14:04:54 +0000

    additional lds and gsea

---
+++ 2_cell_topic_v_beta/scripts/fig_1_gsea.R
@@ -0,0 +1,68 @@
+
+# library(msigdbr)
+
+
+# immunesigdb_human_db <- msigdbr::msigdbr(species = "human",
+# 										category = "C7",
+# 										subcategory = "IMMUNESIGDB")
+# immunesigdb_human_db = as.data.frame(immunesigdb_human_db)
+# write.csv(immunesigdb_human_db, file=gzfile("immunesigdb_human_db.csv.gz"))
+
+# hallmark_human_db <- msigdbr::msigdbr(species = "human",
+# 										category = "H")
+# hallmark_human_db = as.data.frame(hallmark_human_db)
+# write.csv(hallmark_human_db, file=gzfile("hallmark_human_db.csv.gz"))
+
+library(ggplot2)
+library(gridExtra)
+library(reshape)
+library(yaml)
+library(pheatmap)
+library(RColorBrewer)
+library(dplyr)
+setwd(box::file())
+source("Util.R")
+
+args = commandArgs(trailingOnly=TRUE)
+args_home ="/home/BCCRC.CA/ssubedi/projects/experiments/spruce_topic/0_augmented_cell_topic_v_beta/"
+config = paste(args_home,"config/",args[1],".yaml",sep="") 
+args = read_yaml(config)
+
+weightmat_phmap_plot_i <- function(args) {
+
+
+
+beta = paste(args_home,args$output,args$cell_topic$out,args$cell_topic$model_info,args$cell_topic$model_id,"_cmark_hypergeom_test.tsv.gz",sep="")
+
+df_beta = read.table(beta, sep = ",", header=TRUE)
+
+df_beta = cast(df_beta,pathway~topic,value.var='pval')
+df_beta = as.data.frame(df_beta)
+rownames(df_beta) = df_beta$pathway
+df_beta = df_beta[,2:dim(df_beta)[2]]
+print(head(df_beta))
+df_beta = t(df_beta)
+
+
+row_order = row.order(df_beta)
+
+df_beta_t = as.data.frame(df_beta)
+df_beta_t$topic = rownames(df_beta)
+df_beta_t = melt(df_beta_t)
+colnames(df_beta_t)=c('row','col','weight')
+col_order = col.order(df_beta_t,row_order)
+
+df_beta = df_beta[,col_order]
+df_beta = df_beta[row_order,]
+
+# df_beta[df_beta < 0.01] = 0
+df_beta[df_beta > 1e-5] = 1
+
+p1 <- pheatmap(t(df_beta),colorRampPalette(c("navy", "grey"))(100),fontsize_row=8,fontsize_col=8,cluster_rows=FALSE,cluster_cols=FALSE,show_colnames=T)
+
+
+f = paste(args_home,args$output,args$cell_topic$out,args$cell_topic$model_info,args$cell_topic$model_id,"_cell_topic_cmark.pdf",sep="")
+ggsave(f,p1)
+
+}
+weightmat_phmap_plot_i(args)
---
+++ 2_cell_topic_v_beta/sprucetopic/analysis/_gsea.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import hypergeom
+
+def beta_z_score(df_mean,df_sd,smean=0.0,n=1e-3):
+	return (df_mean-smean)/(df_sd/np.sqrt(n))
+
+def get_degs(genes,dfz,cis = 2.575):
+	return [genes[ [ y for y,x in enumerate(dfz.iloc[t,:].values) if abs(x) > cis ] ] for t in dfz.index ]
+
+def hypergeom_test(sp,df_gset):
+
+	dfz = beta_z_score(sp.cell_topic.beta_mean,np.sqrt(np.exp(sp.cell_topic.beta_var)))
+	topic_degs = get_degs(sp.data.raw_data_genes,dfz)
+
+	N=len(sp.data.raw_data_genes)
+	gse = []
+	for pathway in df_gset['gs_name'].unique():
+		pathway_genes = df_gset[df_gset['gs_name']==pathway]['gene_symbol'].values
+		m = len(pathway_genes)
+		for ti,t_deg in enumerate(topic_degs):
+			k = len(t_deg)
+			q = len([x for x in t_deg if x in pathway_genes])
+			pval = 1- hypergeom.cdf(q,N,m,k)
+			gse.append([pathway,ti,pval])
+	return pd.DataFrame(gse,columns=['pathway','topic','pval'])
